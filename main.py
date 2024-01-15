9f1f5eed43144e38c433dd76345c98a493d799c3
Eugenia Subirons <eugenia.subirons@gmail.com>
2024-01-15 23:26:28 +0100

    Recontrucció del codi del guillem amb clases

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 
+import time
+import random
+from mastodon_api import MastodonApi #Mastodon API conection class
+from get_toot import GetToot # Get toot from public timeline class
 
-#probando dos
+#Initialize the instance
+mastodon = MastodonApi().mastodon()
+
+while True:
+    toots = GetToot().get_toot() #obtaining toots data
+
+    # Pick a random toot
+    if toots:
+        last_toot = toots[-1] #taking last toot
+        print(f"{last_toot['account']['username']}: {last_toot['content']}")
+    time.sleep(2)
