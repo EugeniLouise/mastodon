d449aa32cd133ffaf81a4ce06cbc307b5b613fe5
Eugenia Subirons <eugenia.subirons@gmail.com>
2024-01-15 23:08:59 +0100

    Mastodon API conection class

--- mastodon_api.py
+++ mastodon_api.py
@@ -4,7 +4,7 @@ from mastodon import Mastodon
 
 # Initialize Mastodon API
 mastodon = Mastodon(
-    client_id='qkIKn3ytvhkdNQGMPuRQt14Zw0nKCmTMhWfexRHpWIc',
+    client_id='qkIKn3ytvhkdNQGMPuRQt14Zw0nKCmTMhWfexRHpWIc', #que_coño_es_todo_esto
     client_secret='TkHuLSunv4Y6WZf6HAh_07ZHYHfIIzPdGJtzZWuJeE8',
     access_token='X0FwiBlDTKKo-FUAZ7uc26IE_cPZI1D0tFZMUzrgvP8',
     api_base_url='https://mastodon.social'
