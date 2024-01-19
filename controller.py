a8af552227367832516cad101cefb58e19e13e92
Eugenia Subirons <eugenia.subirons@gmail.com>
2024-01-19 21:24:50 +0100

    añadidos cambios pero estan mal

--- controller.py
+++ controller.py
@@ -4,6 +4,12 @@
 class Controller:
     pass
 
+"""
+1. call api
+mastodon_api = MastodonProxy().mastodon()
+
+"""
+
 
 #clase que crida a totes les altres per nar fent.
 #en el main: crida al controller i executa by time sleep.
--- domain/mastodon_toot.py
+++ domain/mastodon_toot.py
@@ -1,9 +1,18 @@
 
-
 class Toot:
-    pass
+    def __init__(self):
+        self.toot_id = toot_id
+        self.toot_language = toot_language
+        self.toot_username = toot_username
+        self.toot_created_at = toot_created_at
+        self.toot_content = toot_content
 
-
-
+"""
+toot['id']
+toot['language']
+toot['account']['username']
+toot['content']
+toot['created_at']
+"""
 
 #definim la entiti toot amb l'init de tot els outputs que volem
