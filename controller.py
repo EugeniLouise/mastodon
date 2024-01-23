7ccca0897d82472689fde2c7491a2798d775109f
Eugenia Subirons <eugenia.subirons@gmail.com>
2024-01-23 23:51:07 +0100

    just get toots

--- controller.py
+++ controller.py
@@ -1,8 +1,26 @@
 
 
+from proxies.mastodon_proxy import MastodonProxy
 
-class Controller:
-    pass
+# Orquestador que utiliza MastodonProxy para obtener toots
+class Orchestrator:
+    def __init__(self, mastodon_proxy):
+        self.mastodon_proxy = mastodon_proxy
+
+    def get_recent_toots(self):
+        # Llama a MastodonProxy para obtener los toots más recientes
+        return self.mastodon_proxy.get_toot()
+
+
+# Uso del código
+mastodon_proxy = MastodonProxy()
+toot_orchestrator = Orchestrator(mastodon_proxy)
+
+recent_toots = toot_orchestrator.get_recent_toots()
+
+# Mostrar los toots
+for toot in recent_toots:
+    print(f"{toot['account']['username']}: {toot['content']}")
 
 """
 1. call api
