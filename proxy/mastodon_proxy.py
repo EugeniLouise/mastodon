0bcdeb6df948fd4d52891b584be8a8b163aed531
Eugenia Subirons <eugenia.subirons@gmail.com>
2024-02-03 16:42:50 +0100

    credentials hidden

--- proxy/mastodon_proxy.py
+++ proxy/mastodon_proxy.py
@@ -1,12 +1,17 @@
 
 from mastodon import Mastodon
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='C:/Users/Eugenia Subirons/PycharmProjects/Mastodon_G/proxy/credentials.env')
 
 # Initialize Mastodon API
 mastodon = Mastodon(
-    client_id='qkIKn3ytvhkdNQGMPuRQt14Zw0nKCmTMhWfexRHpWIc',
-    client_secret='TkHuLSunv4Y6WZf6HAh_07ZHYHfIIzPdGJtzZWuJeE8',
-    access_token='X0FwiBlDTKKo-FUAZ7uc26IE_cPZI1D0tFZMUzrgvP8',
-    api_base_url='https://mastodon.social')
+    client_id=os.getenv('MASTODON_CLIENT_ID'),
+    client_secret=os.getenv('MASTODON_CLIENT_SECRET'),
+    access_token=os.getenv('MASTODON_ACCESS_TOKEN'),
+    api_base_url=os.getenv('MASTODON_API_BASE_URL')
+)
 
 class MastodonProxy:
     def __init__(self):
@@ -15,3 +20,4 @@ class MastodonProxy:
     def get_latest_toots(self, limit=50):
         toots = self.mastodon.timeline_public(limit=limit)
         return toots
+
