896c9faa915b65351309f08afabb27cac3712b8f
Eugenia Subirons <eugenia.subirons@gmail.com>
2024-02-04 16:07:30 +0100

    exception and clarity added

--- repository/toot_repository.py
+++ repository/toot_repository.py
@@ -21,10 +21,15 @@ class TootRepository:
             try:
                 existing_toots_df = pd.read_excel(filename)
                 df = pd.concat([existing_toots_df, new_toots_df], ignore_index=True)
+                print(f"Data added to existing file {filename}.")
             except Exception as e:
-                print(f"Error al agregar datos al archivo existente: {e}")
+                print(f"Error adding data to existing file: {e}")
 
         else:
-            df = new_toots_df
+            try:
+                df = new_toots_df
+                print(f"Data added to file {filename}.")
+            except Exception as e:
+                print(f"Error al crear el archivo {filename}: {e}")
 
         df.to_excel(filename, index=False)
