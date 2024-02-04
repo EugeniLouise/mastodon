5cc0ffceec624d19190cd7e49d53231284750cd3
Eugenia Subirons <eugenia.subirons@gmail.com>
2024-02-04 16:18:02 +0100

    clarity added

--- controller.py
+++ controller.py
@@ -1,10 +1,9 @@
-
-
 from proxy.mastodon_proxy import MastodonProxy
-from uses_cases.toot_collector import TootCollector
-from uses_cases.toot_by_language import TootByLanguage
-from repository.toot_repository import TootRepository
 from repository.language_stats_repository import LanguageStatsRepository
+from repository.toot_repository import TootRepository
+from uses_cases.toot_by_language import TootByLanguage
+from uses_cases.toot_collector import TootCollector
+
 
 class TootController:
     def __init__(self):
@@ -13,8 +12,15 @@ class TootController:
         self.toot_saver = TootRepository()
         self.language_stats_saver = LanguageStatsRepository()
 
-    def run(self):
+    def collect_and_save_toots(self):
         toots = self.toot_collector.collect_latest_toots()
         self.toot_saver.save_toots_to_excel(toots)
+
+    def calculate_and_save_language_stats(self, toots):
         language_stats = TootByLanguage(toots).calculate_language_stats()
         self.language_stats_saver.save_language_stats_to_excel(language_stats)
+
+    def run(self):
+        toots = self.toot_collector.collect_latest_toots()
+        self.collect_and_save_toots()
+        self.calculate_and_save_language_stats(toots)
