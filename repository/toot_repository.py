3826f5f131f05eb9ac460335d11b0a283411f592
Eugenia Subirons <eugenia.subirons@gmail.com>
2024-02-04 16:01:22 +0100

    exception and clarity added

--- repository/toot_repository.py
+++ repository/toot_repository.py
@@ -5,18 +5,26 @@ import pandas as pd
 
 class TootRepository:
     def save_toots_to_excel(self, toots, filename="toots.xlsx"):
-        new_df = pd.DataFrame([{
-            'ID': toot.id,
-            'Language': toot.language,
-            'Username': toot.username,
-            'Created At': toot.created_at,
-            'Content': toot.content
-        } for toot in toots])
+        data = [
+            {
+                'ID': toot.id,
+                'Language': toot.language,
+                'Username': toot.username,
+                'Created At': toot.created_at,
+                'Content': toot.content
+            }
+            for toot in toots]
+
+        new_toots_df = pd.DataFrame(data)
 
         if os.path.exists(filename):
-            existing_df = pd.read_excel(filename)
-            df = pd.concat([existing_df, new_df], ignore_index=True)
+            try:
+                existing_toots_df = pd.read_excel(filename)
+                df = pd.concat([existing_toots_df, new_toots_df], ignore_index=True)
+            except Exception as e:
+                print(f"Error al agregar datos al archivo existente: {e}")
+
         else:
-            df = new_df
+            df = new_toots_df
 
         df.to_excel(filename, index=False)
