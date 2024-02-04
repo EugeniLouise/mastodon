0fae0757585ecebe2a7de2413f98d7af8b398e8c
Eugenia Subirons <eugenia.subirons@gmail.com>
2024-02-04 16:18:33 +0100

    final version

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-
-
 from controller import TootController
+
 
 def main():
     controller = TootController()
     controller.run()
 
+
 if __name__ == "__main__":
     main()
 
-
-#Revisar archivos y entenderlos
-#Rename variables for easy understanding
+# No es CSV, es XLSM, mea culpa.
