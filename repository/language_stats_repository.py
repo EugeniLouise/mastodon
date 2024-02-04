a90bf3fcf2c9a82b7d13968d407343ba8c9aaf12
Eugenia Subirons <eugenia.subirons@gmail.com>
2024-02-04 15:58:23 +0100

    Reformat and clarity added

--- repository/language_stats_repository.py
+++ repository/language_stats_repository.py
@@ -1,8 +1,20 @@
-
-
 import pandas as pd
+
 
 class LanguageStatsRepository:
     def save_language_stats_to_excel(self, language_stats, filename="language_stats.xlsx"):
-        df = pd.DataFrame([{'Language': language, 'Count': stats['count'], 'Last User': stats['last_user']} for language, stats in language_stats.items()])
-        df.to_excel(filename, index=False)
+        data = [
+            {
+                'Language': language,
+                'Count': stats['count'],
+                'Last User': stats['last_user']
+            }
+            for language, stats in language_stats.items()
+        ]
+        df_language_stats = pd.DataFrame(data)
+
+        try:
+            df_language_stats.to_excel(filename, index=False)
+            print(f"Estadísticas guardadas exitosamente en {filename}.")
+        except Exception as e:
+            print(f"Error al guardar las estadísticas en {filename}: {e}")
