573257c0e63969c33def92ef377f46ba1456a33f
Eugenia Subirons <eugenia.subirons@gmail.com>
2024-02-04 15:24:01 +0100

    Reformat added

--- uses_cases/toot_collector.py
+++ uses_cases/toot_collector.py
@@ -1,6 +1,7 @@
+from datetime import timedelta
 
 from domain.toots import Toot
-from datetime import timedelta
+
 
 class TootCollector:
     def __init__(self, mastodon_proxy):
@@ -10,13 +11,12 @@ class TootCollector:
         raw_toots = self.mastodon_proxy.get_latest_toots()
         toots = []
         for raw_toot in raw_toots:
-            # Asegurarse de que created_at sea "naive" y ajustar la hora
             created_at_adjusted = raw_toot['created_at'].replace(tzinfo=None) + timedelta(hours=1)
             toot = Toot(
                 id=raw_toot['id'],
                 content=raw_toot['content'],
                 username=raw_toot['account']['username'],
-                language=raw_toot.get('language', 'unknown'),
+                language=raw_toot.get('language', 'unkown'),
                 created_at=created_at_adjusted
             )
             toots.append(toot)
