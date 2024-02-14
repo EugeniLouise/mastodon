b39b1bc3238086283c968673f293bcf841eea7b9
Eugenia Subirons <eugenia.subirons@gmail.com>
2024-02-14 23:02:34 +0100

    exceptions added, parameters' name changed, and if/else avoided

--- repository/toot_repository.py
+++ repository/toot_repository.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 class TootRepository:
     def save_toots_to_excel(self, toots, filename="toots.xlsx"):
-        data = [  # TODO: què conté data??
+        toots_parameters = [
             {
                 'ID': toot.id,
                 'Language': toot.language,
@@ -15,39 +15,8 @@ class TootRepository:
             }
             for toot in toots]
 
-        new_toots_df = pd.DataFrame(data)
-
-        if os.path.exists(filename):
-            try:
-                existing_toots_df = pd.read_excel(filename)
-                df = pd.concat([existing_toots_df, new_toots_df], ignore_index=True)
-                df.to_excel(filename, index=False)
-                print(f"Data added to existing file {filename}.")
-
-            except Exception as e:  # TODO: classificar comportaments segons tipus d'excepció
-                print(f"Error adding data to existing file: {e}")
-
-        else:
-            try:
-                df = new_toots_df
-                print(f"Data added to file {filename}.")
-                df.to_excel(filename, index=False)
-
-            except Exception as e:
-                print(f"Error al crear el archivo {filename}: {e}")
-
-    def save_toots_to_excel_guilli(self, toots, filename="toots.xlsx"):
-        data = [  # TODO: què conté data??
-            {
-                'ID': toot.id,
-                'Language': toot.language,
-                'Username': toot.username,
-                'Created At': toot.created_at,
-                'Content': toot.content
-            }
-            for toot in toots]
-
-        new_toots_df = pd.DataFrame(data)
+        new_toots_df = pd.DataFrame(toots_parameters)
+        # Evitar els if/elif/else modificant el codi
 
         try:
             if os.path.exists(filename):
@@ -56,5 +25,17 @@ class TootRepository:
             new_toots_df.to_excel(filename, index=False)
             print(f"Data added to file {filename}.")
 
-        except Exception as e:
+        except FileExistsError as fee:
+            print(f"Error with file existence: {fee}")
+
+        except ExcelReadError as ere:
+            print(f"Error reading excel: {ere}")
+
+        except DataFrameConcatError as dfce:
+            print(f"Error with dataframe concat: {dfce}")
+
+        except ExcelWriteError as ewe:
+            print(f"Error writing data to Excel file {filename}: {ewe}")
+
+        except Exception as e: # Excepción genérica
             print(f"Error adding data to file: {e}")
