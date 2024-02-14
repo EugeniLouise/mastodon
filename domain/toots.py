782608669459211d94e054099b9957a3b97f698d
Eugenia Subirons <eugenia.subirons@gmail.com>
2024-02-14 22:23:38 +0100

    class name match file name.

--- domain/toots.py
+++ domain/toots.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 
-class Toot: # TODO: nom class no match amb nom file
+class Toots:
     def __init__(self, id: str, content: str, username: str, language: str, created_at: datetime):
         self.id = id
         self.content = content
